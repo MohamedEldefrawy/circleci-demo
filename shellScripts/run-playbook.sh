17bc1ab3c2a22c133df98f18f58c69e16c3003d2
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-02-17 22:40:15 +0200

    Promote to producction

--- shellScripts/run-playbook.sh
+++ shellScripts/run-playbook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Get all EC2 instance hostnames
-echo "[all]" > playbook/inventory
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text --region us-east-1 >> playbook/inventory
+echo "[all]" >playbook/inventory
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text --region us-east-1 >>playbook/inventory
 
 # Run ansible with the created inventory file
 ansible-playbook -i playbook/inventory playbook/main.yml
