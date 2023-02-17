5da3a5f019fdf91fd1736e2ff5071746610db8e2
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-02-17 18:54:44 +0200

    fix inventory path in sh script

--- shellScripts/run-playbook.sh
+++ shellScripts/run-playbook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Get all EC2 instance hostnames
-echo "[all]" >../playbook/inventory
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text --region us-west-2 >> inventory
+echo "[all]" > playbook/inventory
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text --region us-west-2 >> playbook/inventory
 
 # Run ansible with the created inventory file
 ansible-playbook -i inventory main.yml
