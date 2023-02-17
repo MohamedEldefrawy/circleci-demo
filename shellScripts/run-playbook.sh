948079b2b07446561ecbfa509b07d39103d3f7db
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-02-17 20:06:34 +0200

    change region

--- shellScripts/run-playbook.sh
+++ shellScripts/run-playbook.sh
@@ -2,7 +2,7 @@
 
 # Get all EC2 instance hostnames
 echo "[all]" > playbook/inventory
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text --region us-west-2 >> playbook/inventory
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text --region us-east-1 >> playbook/inventory
 
 # Run ansible with the created inventory file
 ansible-playbook -i playbook/inventory playbook/main.yml
