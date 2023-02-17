ee7c63bdec93c8d5331e08448f61c94dcd893e6c
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-02-17 18:59:02 +0200

    Fix ansible-playbook path

--- shellScripts/run-playbook.sh
+++ shellScripts/run-playbook.sh
@@ -5,4 +5,4 @@ echo "[all]" > playbook/inventory
 aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text --region us-west-2 >> playbook/inventory
 
 # Run ansible with the created inventory file
-ansible-playbook -i inventory main.yml
+ansible-playbook -i playbook/inventory playbook/main.yml
