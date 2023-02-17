952c6aac5901c03864d3372ba3cba0647c4c87dd
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-02-17 18:47:32 +0200

    fix aws describe-instances command

--- shellScripts/run-playbook.sh
+++ shellScripts/run-playbook.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Get all EC2 instance hostnames
-hostnames=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicDnsName' --output text)
-
-# Create inventory file with the hostnames
-echo "[ec2_hosts]" >../playbook/inventory
+echo "[all]" >../playbook/inventory
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text --region us-west-2 >> inventory
 
 # Run ansible with the created inventory file
 ansible-playbook -i inventory main.yml
